65f3bddefc27da2c23375dc565635a802d3eb5d7
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-12-23 22:23:02 +0200

    Refine unit tests

--- tests/EndpointMatcher.Tests/EndpointMatcherTest.cs
+++ tests/EndpointMatcher.Tests/EndpointMatcherTest.cs
@@ -9,9 +9,15 @@ namespace EndpointMatcher.Tests
         public void GivenEndpointMatcherWhenSingleRouteExistsThenReturnMatchedRoute()
         {
             // Arrange
-            Dictionary<string, List<string>> routes = new Dictionary<string, List<string>>
+            var routes = new Dictionary<string, List<string>>
             {
-                { "users", new List<string>() { "users/{name}", "users/{id}/applications" } }
+                {
+                    "users",
+                    new List<string>()
+                    {
+                        "users/{name}", "users/{id}/applications"
+                    }
+                }
             };
 
             var endpointMatcher = new EndpointMatcher(routes);
@@ -27,11 +33,32 @@ namespace EndpointMatcher.Tests
         public void GivenEndpointMatcherWhenLongRouteExistsThenReturnMatchedRoute()
         {
             // Arrange
-            Dictionary<string, List<string>> routes = new Dictionary<string, List<string>>
+            var routes = new Dictionary<string, List<string>>
             {
-                { "users", new List<string>() { "users/{name}", "users/{id}/applications", "users/{id:number}" } },
-                { "categories", new List<string>() { "categories/{id}/apps/{appId}" } },
-                { "pages", new List<string>() { "pages/{id:number}/sections/myPages", "pages/{id}/sections" } }
+                {
+                    "users",
+                    new List<string>()
+                    {
+                        "users/{name}",
+                        "users/{id}/applications",
+                        "users/{id:number}"
+                    }
+                },
+                {
+                    "categories",
+                    new List<string>()
+                    {
+                        "categories/{id}/apps/{appId}"
+                    }
+                },
+                {
+                    "pages",
+                    new List<string>()
+                    {
+                        "pages/{id:number}/sections/myPages",
+                        "pages/{id}/sections"
+                    }
+                }
             };
 
             var endpointMatcher = new EndpointMatcher(routes);
@@ -47,11 +74,32 @@ namespace EndpointMatcher.Tests
         public void GivenEndpointMatcherWhenManyPossibleRoutesExistThenReturnMatchedRoute()
         {
             // Arrange
-            Dictionary<string, List<string>> routes = new Dictionary<string, List<string>>
+            var routes = new Dictionary<string, List<string>>
             {
-                { "users", new List<string>() { "users/{name:string}", "users/{id:number}", "users/signout" } },
-                { "categories", new List<string>() { "categories/{id}/apps/{appId}" } },
-                { "pages", new List<string>() { "pages/{id:number}/sections/myPages", "pages/{id}/sections" } }
+                {
+                    "users",
+                    new List<string>()
+                    {
+                        "users/{name:string}",
+                        "users/{id:number}",
+                        "users/signout"
+                    }
+                },
+                {
+                    "categories",
+                    new List<string>()
+                    {
+                        "categories/{id}/apps/{appId}"
+                    }
+                },
+                {
+                    "pages",
+                    new List<string>()
+                    {
+                        "pages/{id:number}/sections/myPages",
+                        "pages/{id}/sections"
+                    }
+                }
             };
 
             var endpointMatcher = new EndpointMatcher(routes);
@@ -67,7 +115,7 @@ namespace EndpointMatcher.Tests
         public void GivenEndpointMatcherWhenStraightRouteExistsThenReturnMatchedRoute()
         {
             // Arrange
-            Dictionary<string, List<string>> routes = new Dictionary<string, List<string>>
+            var routes = new Dictionary<string, List<string>>
             {
                 {
                     "users",
@@ -88,6 +136,33 @@ namespace EndpointMatcher.Tests
 
             // Assert
             Assert.Equal("users/signout/pages/applications", result);
+        }
+
+        [Fact]
+        public void GivenEndpointMatcherWhenManySpecificRoutesExistThenReturnMatchedRoute()
+        {
+            // Arrange
+            var routes = new Dictionary<string, List<string>>
+            {
+                {
+                    "users",
+                    new List<string>()
+                    {
+                        "users/signout/pages/{name:string}",
+                        "users/{name:string}",
+                        "users/{id:number}/pages/applications/{applicationId}",
+                        "users/{id:number}/pages/applications/{applicationId:number}"
+                    }
+                }
+            };
+
+            var endpointMatcher = new EndpointMatcher(routes);
+
+            // Act
+            var result = endpointMatcher.Match("users/22/pages/applications/3");
+
+            // Assert
+            Assert.Equal("users/{id:number}/pages/applications/{applicationId:number}", result);
         }
     }
 }
